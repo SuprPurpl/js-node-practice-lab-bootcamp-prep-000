f9b53df7ef2efe39d4ce3b3d2043d9b4bc656d9f
Ethan Murphy <verakai141@gmail.com>
2018-07-26 12:50:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // write your code below!
 var name = "Ethan";
-var height = "";
+var height = "74";
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseInt(height) - 2)
